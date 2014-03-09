d26839f6f5d05a05f35127e974482b429b6931c7
Ruba Merza <ruba.merza@gmail.com>
2014-03-09 01:02:34 -0600

    Using Popen to execute and capture output of traceroute command

--- maproute.py
+++ maproute.py
@@ -1,11 +1,22 @@
-from subprocess import call
+import subprocess
+from subprocess import Popen, PIPE 
 from sys import argv
 
 
 def main():
     dest = argv[1]
     print "host to traceroute is: %s" % dest
-    call(["traceroute", dest])
-
+    args = ['traceroute', dest]
+    print args
+    process = subprocess.Popen(args, stdout=PIPE)
+    print process
+    (output, err) = process.communicate()
+    rc = process.poll()
+    print rc
+    '''
+        not sure if I should be dealing with an interrupted traceroute
+        if rc < 0:
+            print "traceroute terminated"
+    '''
 if __name__ == '__main__':
     main()
