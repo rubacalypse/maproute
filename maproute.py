3791cd976cce916bfba3b5d8bf6183342461b8ba
Ruba Merza <ruba.merza@gmail.com>
2014-04-18 21:04:01 -0500

    put traceroute call in its own function and replaced subprocess Popen with check_output

--- maproute.py
+++ maproute.py
@@ -1,22 +1,16 @@
-import subprocess
-from subprocess import Popen, PIPE 
+from subprocess import call, check_output, CalledProcessError
 from sys import argv
 
 
 def main():
-    dest = argv[1]
-    print "host to traceroute is: %s" % dest
-    args = ['traceroute', dest]
-    print args
-    process = subprocess.Popen(args, stdout=PIPE)
-    print process
-    (output, err) = process.communicate()
-    rc = process.poll()
-    print rc
-    '''
-        not sure if I should be dealing with an interrupted traceroute
-        if rc < 0:
-            print "traceroute terminated"
-    '''
+    arg = argv[1]
+    perform_traceroute(arg)
+
+
+def perform_traceroute(arg):
+    output = check_output("traceroute %s" % arg, shell=True)
+    routes = output.split("\n")
+    print routes[1:]
+
 if __name__ == '__main__':
     main()
