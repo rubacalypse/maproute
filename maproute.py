52a62b0847d19f76e13510e3c24e04756381cee5
Ruba Merza <ruba.merza@gmail.com>
2014-05-30 18:58:25 -0500

    added get_ips

--- maproute.py
+++ maproute.py
@@ -1,4 +1,5 @@
-from subprocess import call, check_output, CalledProcessError
+import pygeoip
+from subprocess import check_output
 from sys import argv
 
 
@@ -9,8 +10,32 @@ def main():
 
 def perform_traceroute(arg):
     output = check_output("traceroute %s" % arg, shell=True)
-    routes = output.split("\n")
-    print routes[1:]
+    hops = output.split("\n")
+    ips = get_ips(hops[1:])
+    print ips
+    geo = []
+    for hop in hops[1:]:
+        geo.append(hop)
+    #print geo
+    #print "\n".join(hops[1:])
+#libaa libcaca geoip
+
+
+def get_ips(hops):
+    ips = [(hop.split())[2] for hop in hops if hop != '' and hop != '*']
+    print ips
+    return ips
+
+
+def ip_to_location(ip):
+    gi = pygeoip.GeoIP("/usr/share/GeoIP/GeoLiteCity.dat")
+    output = gi.record_by_addr(ip)
+    city = output['city']
+    region = output['region_code']
+    country = output['country_name']
+    return city, region, country
+
 
 if __name__ == '__main__':
     main()
+
